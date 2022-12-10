5ca57f3363f3642084928a2cb2351cae8a0a5465
TomsProject <44722363+TomsProject@users.noreply.github.com>
2022-12-10 16:44:52 +0800

    Update index.js

--- documentation/src/theme/Footer/Layout/index.js
+++ documentation/src/theme/Footer/Layout/index.js
@@ -20,19 +20,6 @@ const LoveIcon = (props) => (
     </svg>
 );
 
-const DiscordIcon = (props) => (
-    <svg
-        width={24}
-        height={19}
-        viewBox="0 0 24 19"
-        fill="#9696B4"
-        xmlns="http://www.w3.org/2000/svg"
-        {...props}
-    >
-        <path d="M17.712 19s-.74-.973-1.356-1.832c2.692-.837 3.72-2.692 3.72-2.692a11.39 11.39 0 0 1-2.364 1.335 12.61 12.61 0 0 1-2.98.972c-1.972.407-3.78.294-5.321-.022a16.074 16.074 0 0 1-3.02-.973 11.563 11.563 0 0 1-1.5-.769c-.062-.045-.124-.068-.186-.113a.287.287 0 0 1-.082-.068c-.37-.226-.575-.384-.575-.384s.986 1.81 3.596 2.669C7.027 17.983 6.267 19 6.267 19 1.726 18.842 0 15.562 0 15.562 0 8.279 2.959 2.375 2.959 2.375 5.918-.068 8.733 0 8.733 0l.205.272C5.24 1.448 3.534 3.235 3.534 3.235s.452-.272 1.213-.656C6.945 1.516 8.692 1.222 9.41 1.154c.123-.023.226-.045.35-.045a15.807 15.807 0 0 1 4.15-.046c1.952.25 4.048.882 6.185 2.172 0 0-1.623-1.697-5.117-2.873L15.268 0s2.815-.068 5.774 2.375c0 0 2.959 5.904 2.959 13.187 0 0-1.747 3.28-6.288 3.438ZM8.158 8.437c-1.172 0-2.096 1.131-2.096 2.51 0 1.38.945 2.511 2.096 2.511 1.17 0 2.095-1.13 2.095-2.51.021-1.38-.924-2.511-2.095-2.511Zm7.5 0c-1.172 0-2.096 1.131-2.096 2.51 0 1.38.945 2.511 2.096 2.511 1.17 0 2.095-1.13 2.095-2.51s-.924-2.511-2.095-2.511Z" />
-    </svg>
-);
-
 const TwitterIcon = (props) => (
     <svg
         width={24}
@@ -139,33 +126,6 @@ export default function FooterLayout({
                         </div>
                     )}
 
-                    <a
-                        href="https://www.producthunt.com/posts/refine-3?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-refine&#0045;3"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <img
-                            src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=362220&theme=neutral&period=daily"
-                            alt="refine - 100&#0037;&#0032;open&#0032;source&#0032;React&#0032;framework&#0032;to&#0032;build&#0032;web&#0032;apps&#0032;3x&#0032;faster | Product Hunt"
-                            style={{ width: "166px", height: "35px" }}
-                            width="250"
-                            height="54"
-                        />
-                    </a>
-
-                    <a
-                        href="https://github.com/sponsors/refinedev"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <iframe
-                            src="https://github.com/sponsors/refinedev/button"
-                            title="Sponsor refinedev"
-                            height="35"
-                            width="116"
-                            style={{ border: 0 }}
-                        ></iframe>
-                    </a>
                 </div>
                 <div className="flex flex-col gap-6 lg:flex-row lg:gap-20">
                     <div
@@ -219,9 +179,6 @@ export default function FooterLayout({
                                     )}
                                     {socialLink.label === "linkedin" && (
                                         <LinkedinIcon className="h-[22px] w-[22px] transition-colors duration-500 ease-in-out hover:fill-[#0077B5]" />
-                                    )}
-                                    {socialLink.label === "discord" && (
-                                        <DiscordIcon className="h-[22px] w-[22px] transition-colors duration-500 ease-in-out hover:fill-[#5865F2]" />
                                     )}
                                     {socialLink.label === "reddit" && (
                                         <RedditIcon className="h-[22px] w-[22px] transition-colors duration-500 ease-in-out hover:fill-[#FF4300]" />
