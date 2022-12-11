d5d9b3d0ff4676acbf99a55cf171725eb63ff9ae
TomsProject <44722363+TomsProject@users.noreply.github.com>
2022-12-11 18:14:11 +0800

    Delete examples/i18n directory

--- examples/i18n/nextjs/next.config.js
+++
@@ -1,9 +0,0 @@
-const withPlugins = require("next-compose-plugins");
-const { i18n } = require("./next-i18next.config");
-
-module.exports = withPlugins([], {
-    i18n,
-    experimental: {
-        newNextLinkBehavior: true,
-    },
-});
