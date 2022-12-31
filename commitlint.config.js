b10dd9184ba85ae762ea176ee47ff5ba368bb8c1
TomsProject <44722363+TomsProject@users.noreply.github.com>
2022-12-31 11:40:32 +0800

    Delete commitlint.config.js

--- commitlint.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    extends: ["@commitlint/config-conventional"],
-    ignores: [
-        (commit) =>
-            commit.includes("Optimised images with calibre/image-actions"),
-    ],
-};
