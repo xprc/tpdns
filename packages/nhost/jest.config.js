7f857c7c24fb5d0b7a312c6b25701e1b980a3de3
TomsProject <44722363+TomsProject@users.noreply.github.com>
2022-12-31 12:21:35 +0800

    Delete packages/nhost directory

--- packages/nhost/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: "ts-jest",
-    rootDir: "./",
-    name: "refine-nhost",
-    displayName: "refine-nhost",
-    setupFilesAfterEnv: ["<rootDir>/test/jest.setup.js"],
-    testEnvironment: "jsdom",
-};
