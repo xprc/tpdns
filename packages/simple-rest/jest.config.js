a94243d1c65f90ab61a4b45b52f12695160fffec
TomsProject <44722363+TomsProject@users.noreply.github.com>
2022-12-31 12:23:48 +0800

    Delete packages/simple-rest directory

--- packages/simple-rest/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: "ts-jest",
-    rootDir: "./",
-    name: "simple-rest",
-    displayName: "simple-rest",
-    setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
-    testEnvironment: "jsdom",
-};
