68af71a7d2255bf2c89ebbfe88508e86c7b433b7
TomsProject <44722363+TomsProject@users.noreply.github.com>
2022-12-31 12:23:57 +0800

    Delete packages/strapi directory

--- packages/strapi/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: "ts-jest",
-    rootDir: "./",
-    name: "strapi",
-    displayName: "strapi",
-    setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
-    testEnvironment: "jsdom",
-};
