bb74ab2343108653b3ec25c755e941a78448b8a3
TomsProject <44722363+TomsProject@users.noreply.github.com>
2022-12-31 12:23:45 +0800

    Delete packages/sdk directory

--- packages/sdk/src/services/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./api/log";
-export * from "./auth";
-export * from "./log";
-export * from "./draft-resource";
-export * from "./config";
-export * from "./cloud-query";
-export * from "./storage";
