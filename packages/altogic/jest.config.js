fd50fb79cdba913261bbad58a510d185acc30c5e
TomsProject <44722363+TomsProject@users.noreply.github.com>
2022-12-31 12:08:07 +0800

    Delete packages/altogic directory

--- packages/altogic/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: "ts-jest",
-    rootDir: "./",
-    name: "altogic",
-    displayName: "altogic",
-    setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
-    testEnvironment: "jsdom",
-};
