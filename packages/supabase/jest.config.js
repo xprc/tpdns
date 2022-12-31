227e11c96f54afde52a890e07dea28e6fb6d5ab8
TomsProject <44722363+TomsProject@users.noreply.github.com>
2022-12-31 12:24:00 +0800

    Delete packages/supabase directory

--- packages/supabase/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: "ts-jest",
-    rootDir: "./",
-    name: "supabase",
-    displayName: "supabase",
-    setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
-    testEnvironment: "jsdom",
-};
