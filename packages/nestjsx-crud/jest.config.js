e765f1acbf8632158bd7e6c0df969ae6944043e6
TomsProject <44722363+TomsProject@users.noreply.github.com>
2022-12-31 12:21:29 +0800

    Delete packages/nestjsx-crud directory

--- packages/nestjsx-crud/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: "ts-jest",
-    rootDir: "./",
-    name: "nestjsx-crud",
-    displayName: "nestjsx-crud",
-    setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
-    testEnvironment: "jsdom",
-};
