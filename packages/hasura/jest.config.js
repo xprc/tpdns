c26d5488255e575035cd84da5b6aaf8487a6e936
TomsProject <44722363+TomsProject@users.noreply.github.com>
2022-12-31 12:19:06 +0800

    Delete packages/hasura directory

--- packages/hasura/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: "ts-jest",
-    rootDir: "./",
-    name: "refine-hasura",
-    displayName: "refine-hasura",
-    setupFilesAfterEnv: ["<rootDir>/test/jest.setup.js"],
-    testEnvironment: "jsdom",
-};
