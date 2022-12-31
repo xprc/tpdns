7d8f5ca64e92a45eef1c3fc895d7b1f89976c7ae
TomsProject <44722363+TomsProject@users.noreply.github.com>
2022-12-31 12:23:52 +0800

    Delete packages/strapi-graphql directory

--- packages/strapi-graphql/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: "ts-jest",
-    rootDir: "./",
-    name: "strapi-graphql",
-    displayName: "strapi-graphql",
-    setupFilesAfterEnv: ["<rootDir>/test/jest.setup.js"],
-    testEnvironment: "jsdom",
-};
